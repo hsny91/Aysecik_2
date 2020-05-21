ddd8e31930ca12b4b05f81657320cba0dc695f70
Husniye Sekeroglu <husniyesakmak.91@gmail.com>
2020-05-21 23:41:30 +0200

    app.js modified

--- app.js
+++ app.js
@@ -17,21 +17,22 @@ let firstArray = [
   "onbeş"
 ]; 
 let degeri = prompt("Ayse Bir Sayi Girer Misin.");
-let newArray = new Array();
+let yeniDizi = new Array();
 for (let i = 0; i < degeri; i++) {
   let deger = prompt("sayi:" + (i + 1));
   let lowerCaseDeger = deger.toLowerCase();
   for (let j = 0; j < firstArray.length; j++) {
     if (firstArray[j] === lowerCaseDeger) {
-      newArray.push(newArray.indexOf(lowerCaseDeger)); 
+      yeniDizi.push(firstArray.indexOf(lowerCaseDeger)); 
     }
   }
 }
+
 girilenSayi(degeri);
-sayiListesi(newArray);
-toplam(newArray);
-carpim(newArray);
-enBuyukSayi(newArray);
-enKucukSayi(newArray);
-tekrarliSayilar(newArray); 
-hataBulma(degeri, newArray);
+sayiListesi(yeniDizi);
+toplam(yeniDizi);
+carpim(yeniDizi);
+enBuyukSayi(yeniDizi);
+enKucukSayi(yeniDizi);
+tekrarliSayilar(yeniDizi); 
+hataBulma(degeri, yeniDizi);
