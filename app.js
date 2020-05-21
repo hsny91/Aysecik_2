40906ba93ae0bcee7363d62aeff5179e081532b8
Husniye Sekeroglu <husniyesakmak.91@gmail.com>
2020-05-21 23:33:07 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+let ilkDizi = [
+  "sifir",
+  "bir",
+  "iki",
+  "üç",
+  "dört",
+  "beş",
+  "altı",
+  "yedi",
+  "sekiz",
+  "dokuz",
+  "on",
+  "onbir",
+  "oniki",
+  "onüç",
+  "ondört",
+  "onbeş"
+]; 
+let degeri = prompt("Ayse Bir Sayi Girer misin.");
+let yeniDizi = new Array();
+for (let i = 0; i < degeri; i++) {
+  let deger = prompt("sayi:" + (i + 1));
+  let lowerCaseDeger = deger.toLowerCase();
+  for (let j = 0; j < ilkDizi.length; j++) {
+    if (ilkDizi[j] === lowerCaseDeger) {
+      yeniDizi.push(ilkDizi.indexOf(lowerCaseDeger)); 
+    }
+  }
+}
+
+girilenSayi(degeri);
+sayiListesi(yeniDizi);
+toplam(yeniDizi);
+carpim(yeniDizi);
+enBuyukSayi(yeniDizi);
+enKucukSayi(yeniDizi);
+tekrarliSayilar(yeniDizi); 
+hataBulma(degeri, yeniDizi);
---
+++ function.js
@@ -0,0 +1,66 @@
+function girilenSayi(sayi) {
+  // kac sayi girildi bilgisini veren fonksiyon
+  document.getElementById("girilenSayi").innerHTML =
+    sayi + " Tane Sayi Girilmistir.";
+}
+
+function sayiListesi(diziAdi) {
+  // Girilen sayilarin listesini veren fonksiyon.
+  document.getElementById("sayiListesi").innerHTML =
+    "Ayse'nin Girdigi Sayilar: " + diziAdi;
+}
+
+function toplam(diziAdi) {
+  let topSonuc = 0;
+  for (let i = 0; i < diziAdi.length; i++) {
+    topSonuc += Number(diziAdi[i]);
+  }
+  document.getElementById("toplam").innerHTML =
+    "Ayse'nin Girdigi Sayilarin Toplami: " + topSonuc;
+}
+
+function carpim(diziAdi) {
+  let carpSonuc = 1;
+  for (let i = 0; i < diziAdi.length; i++) {
+    carpSonuc *= Number(diziAdi[i]);
+  }
+  document.getElementById("carpim").innerHTML =
+    "Ayse'nin Girdigi Sayilarin Carpimi: " + carpSonuc;
+}
+function enBuyukSayi(diziAdi) {
+  let max = Math.max.apply(Math, diziAdi);
+  document.getElementById("enBuyukSayi").innerHTML =
+    "Ayse'nin Girdigi En Buyuk Sayi: " + max;
+}
+
+function enKucukSayi(diziAdi) {
+  let min = Math.min.apply(Math, diziAdi);
+  document.getElementById("enKucukSayi").innerHTML =
+    "Ayse'nin Girdigi En Kucuk Sayi: " + min;
+}
+function hataBulma(ilkDeger, diziAdi) {
+  //  cok dogru bir kodlama olmadi. Normalde ilkDizi ve yeniDizi karakterlerini tek tek karsilastirmayi denedim ancak oldukca uzun oldu bende bunu tecih ettim.
+  // Ayse herhangi bir sayiyi yanlis girdiginde; girilen deger diziye atanmiyor, normalde dizideki eleman sayisi ile Aysenin ilk girdigi sayi ayni olmasi gerekirken dizinin uzunlugu ilk girilen sayidan kucuk oluyor. Ayse yanlis yazi yazdiktan sonra dizilistesinde eksik olan sayiyi fark edip yanlisini bulmasini umdum.
+  if (ilkDeger > diziAdi.length) {
+    // Burda Aysenin girdigi ilk deger ve dizinin uzunlugu karsilastiriliyor.(ilkdeger ayseden alinan ilk sayi, diziAdi girilen sayilarla olusturulan yeniDizi)
+    document.getElementById("hataBulma").innerHTML = "AYSE HATALI SAYI GIRDIN";
+  }
+}
+function tekrarliSayilar(diziAdi) {
+  let k = "";
+  for (let i = 0; i < diziAdi.length; i++) {
+    let sayac = 0;
+    for (let j = 0; j < diziAdi.length; j++) {
+      if (diziAdi[i] === diziAdi[j]) {
+        // if fonksiyonunda amac dizi icerisindeki elamanlari karsilastirmak ve ayni olanlari tespit etmek .
+        sayac++;
+      }
+    }
+    if (sayac != 0) {
+      // sayacimin sifir olmamasi durumu dizinin icinde kendini tekrar eden elemanlarin oldugunu gosterir. Bunlar "k" degiskenin icine atanir.
+      k += document.getElementById("tekrarliSayilar").innerHTML =
+        diziAdi[i] + " Sayisindan " + sayac + " Tane Girilmistir.<br>";
+    }
+    document.getElementById("tekrarliSayilar").innerHTML = k;
+  }
+}
